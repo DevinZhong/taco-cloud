9b787b9dd58560e3c5da17eab497f22d0b4fe469
xiaoshi <xiaoshi@imagedt.com>
2020-03-25 14:08:51 +0800

    feat: 接收 /design 请求

---
+++ src/main/java/com/way2hacker/tacocloud/DesignTacoController.java
@@ -0,0 +1,37 @@
+package com.way2hacker.tacocloud;
+
+import com.way2hacker.tacocloud.data.Ingredient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.PROTEIN));
+
+        Ingredient.Type[] types = Ingredient.Type.values();
+
+        for (Ingredient.Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+        }
+
+//        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
+        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+    }
+}
